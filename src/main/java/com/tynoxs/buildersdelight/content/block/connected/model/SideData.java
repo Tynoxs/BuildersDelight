f2a209ee9f956cf78d1864cfc52a05d0f3926ae9
Benjamin LE SIRE <benjaminlesire@yahoo.com>
2023-01-11 13:00:07 +0100

    Remove ununsed imports

--- src/main/java/com/tynoxs/buildersdelight/content/block/connected/model/SideData.java
+++ src/main/java/com/tynoxs/buildersdelight/content/block/connected/model/SideData.java
@@ -1,19 +1,11 @@
 package com.tynoxs.buildersdelight.content.block.connected.model;
 
-import com.tynoxs.buildersdelight.content.block.connected.IConnectedTextureBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.client.model.data.ModelData;
-import net.minecraftforge.client.model.data.ModelProperty;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.HashMap;
-import java.util.Map;
 
 class SideData {
     private BlockGetter world;
