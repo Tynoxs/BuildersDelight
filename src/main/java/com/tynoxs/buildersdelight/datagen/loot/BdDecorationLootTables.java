f1bc15c964e444972a6aaf9c315a0d67de5ab5ea
Benjamin LE SIRE <benjaminlesire@yahoo.com>
2023-12-11 17:39:34 +0100

    Refactor drops

--- src/main/java/com/tynoxs/buildersdelight/datagen/loot/BdDecorationLootTables.java
+++ src/main/java/com/tynoxs/buildersdelight/datagen/loot/BdDecorationLootTables.java
@@ -9,59 +9,63 @@ public class BdDecorationLootTables extends BlockLoot {
 
     @Override
     protected void addTables() {
-        this.dropSelf(BdDecoration.ACACIA_CHAIR_1.get());
-        this.dropSelf(BdDecoration.ACACIA_CHAIR_2.get());
-        this.dropSelf(BdDecoration.ACACIA_TABLE_1.get());
-        this.dropSelf(BdDecoration.ACACIA_TABLE_2.get());
+        registerDropSelf(BdDecoration.ACACIA_CHAIR_1.get());
+        registerDropSelf(BdDecoration.ACACIA_CHAIR_2.get());
+        registerDropSelf(BdDecoration.ACACIA_TABLE_1.get());
+        registerDropSelf(BdDecoration.ACACIA_TABLE_2.get());
 
-        this.dropSelf(BdDecoration.BIRCH_CHAIR_1.get());
-        this.dropSelf(BdDecoration.BIRCH_CHAIR_2.get());
-        this.dropSelf(BdDecoration.BIRCH_TABLE_1.get());
-        this.dropSelf(BdDecoration.BIRCH_TABLE_2.get());
+        registerDropSelf(BdDecoration.BIRCH_CHAIR_1.get());
+        registerDropSelf(BdDecoration.BIRCH_CHAIR_2.get());
+        registerDropSelf(BdDecoration.BIRCH_TABLE_1.get());
+        registerDropSelf(BdDecoration.BIRCH_TABLE_2.get());
 
-        this.dropSelf(BdDecoration.CRIMSON_CHAIR_1.get());
-        this.dropSelf(BdDecoration.CRIMSON_CHAIR_2.get());
-        this.dropSelf(BdDecoration.CRIMSON_TABLE_1.get());
-        this.dropSelf(BdDecoration.CRIMSON_TABLE_2.get());
+        registerDropSelf(BdDecoration.CRIMSON_CHAIR_1.get());
+        registerDropSelf(BdDecoration.CRIMSON_CHAIR_2.get());
+        registerDropSelf(BdDecoration.CRIMSON_TABLE_1.get());
+        registerDropSelf(BdDecoration.CRIMSON_TABLE_2.get());
 
-        this.dropSelf(BdDecoration.DARK_OAK_CHAIR_1.get());
-        this.dropSelf(BdDecoration.DARK_OAK_CHAIR_2.get());
-        this.dropSelf(BdDecoration.DARK_OAK_TABLE_1.get());
-        this.dropSelf(BdDecoration.DARK_OAK_TABLE_2.get());
+        registerDropSelf(BdDecoration.DARK_OAK_CHAIR_1.get());
+        registerDropSelf(BdDecoration.DARK_OAK_CHAIR_2.get());
+        registerDropSelf(BdDecoration.DARK_OAK_TABLE_1.get());
+        registerDropSelf(BdDecoration.DARK_OAK_TABLE_2.get());
 
-        this.dropSelf(BdDecoration.JUNGLE_CHAIR_1.get());
-        this.dropSelf(BdDecoration.JUNGLE_CHAIR_2.get());
-        this.dropSelf(BdDecoration.JUNGLE_TABLE_1.get());
-        this.dropSelf(BdDecoration.JUNGLE_TABLE_2.get());
+        registerDropSelf(BdDecoration.JUNGLE_CHAIR_1.get());
+        registerDropSelf(BdDecoration.JUNGLE_CHAIR_2.get());
+        registerDropSelf(BdDecoration.JUNGLE_TABLE_1.get());
+        registerDropSelf(BdDecoration.JUNGLE_TABLE_2.get());
 
-        this.dropSelf(BdDecoration.OAK_CHAIR_1.get());
-        this.dropSelf(BdDecoration.OAK_CHAIR_2.get());
-        this.dropSelf(BdDecoration.OAK_TABLE_1.get());
-        this.dropSelf(BdDecoration.OAK_TABLE_2.get());
+        registerDropSelf(BdDecoration.OAK_CHAIR_1.get());
+        registerDropSelf(BdDecoration.OAK_CHAIR_2.get());
+        registerDropSelf(BdDecoration.OAK_TABLE_1.get());
+        registerDropSelf(BdDecoration.OAK_TABLE_2.get());
 
-        this.dropSelf(BdDecoration.SPRUCE_CHAIR_1.get());
-        this.dropSelf(BdDecoration.SPRUCE_CHAIR_2.get());
-        this.dropSelf(BdDecoration.SPRUCE_TABLE_1.get());
-        this.dropSelf(BdDecoration.SPRUCE_TABLE_2.get());
+        registerDropSelf(BdDecoration.SPRUCE_CHAIR_1.get());
+        registerDropSelf(BdDecoration.SPRUCE_CHAIR_2.get());
+        registerDropSelf(BdDecoration.SPRUCE_TABLE_1.get());
+        registerDropSelf(BdDecoration.SPRUCE_TABLE_2.get());
 
-        this.dropSelf(BdDecoration.WARPED_CHAIR_1.get());
-        this.dropSelf(BdDecoration.WARPED_CHAIR_2.get());
-        this.dropSelf(BdDecoration.WARPED_TABLE_1.get());
-        this.dropSelf(BdDecoration.WARPED_TABLE_2.get());
+        registerDropSelf(BdDecoration.WARPED_CHAIR_1.get());
+        registerDropSelf(BdDecoration.WARPED_CHAIR_2.get());
+        registerDropSelf(BdDecoration.WARPED_TABLE_1.get());
+        registerDropSelf(BdDecoration.WARPED_TABLE_2.get());
 
-        this.dropSelf(BdDecoration.CHAIN_1.get());
-        this.dropSelf(BdDecoration.CHAIN_2.get());
-        this.dropSelf(BdDecoration.CHAIN_3.get());
-        this.dropSelf(BdDecoration.CHAIN_4.get());
-        this.dropSelf(BdDecoration.CHAIN_5.get());
+        registerDropSelf(BdDecoration.CHAIN_1.get());
+        registerDropSelf(BdDecoration.CHAIN_2.get());
+        registerDropSelf(BdDecoration.CHAIN_3.get());
+        registerDropSelf(BdDecoration.CHAIN_4.get());
+        registerDropSelf(BdDecoration.CHAIN_5.get());
 
-        this.dropSelf(BdDecoration.LANTERN_1.get());
-        this.dropSelf(BdDecoration.LANTERN_2.get());
-        this.dropSelf(BdDecoration.LANTERN_3.get());
-        this.dropSelf(BdDecoration.LANTERN_4.get());
-        this.dropSelf(BdDecoration.LANTERN_5.get());
-        this.dropSelf(BdDecoration.LANTERN_6.get());
+        registerDropSelf(BdDecoration.LANTERN_1.get());
+        registerDropSelf(BdDecoration.LANTERN_2.get());
+        registerDropSelf(BdDecoration.LANTERN_3.get());
+        registerDropSelf(BdDecoration.LANTERN_4.get());
+        registerDropSelf(BdDecoration.LANTERN_5.get());
+        registerDropSelf(BdDecoration.LANTERN_6.get());
+    }
 
+    public void registerDropSelf(Block block) {
+        list.add(block);
+        dropSelf(block);
     }
 
     @Override
