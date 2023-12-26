f5dbfbf46644511f4ecb72142d8326aa4049e9d6
Nico <tynoxs@gmail.com>
2023-12-26 01:33:06 +0100

    Update BdBlockLootTableProvider.java

--- src/main/java/com/tynoxs/buildersdelight/datagen/providers/BdBlockLootTableProvider.java
+++ src/main/java/com/tynoxs/buildersdelight/datagen/providers/BdBlockLootTableProvider.java
@@ -2,24 +2,22 @@ package com.tynoxs.buildersdelight.datagen.providers;
 
 import com.google.common.collect.ImmutableList;
 import com.tynoxs.buildersdelight.datagen.loot.BdBlockLootTables;
+import com.tynoxs.buildersdelight.datagen.loot.BdDecorationLootTables;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.ValidationContext;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 public class BdBlockLootTableProvider extends LootTableProvider {
-    private final List<SubProviderEntry>
-            block_loot_tables = ImmutableList.of(new LootTableProvider.SubProviderEntry(BdBlockLootTables::new, LootContextParamSets.BLOCK));
+    private final List<SubProviderEntry> block_loot_tables = ImmutableList.of(new LootTableProvider.SubProviderEntry(BdBlockLootTables::new, LootContextParamSets.BLOCK));
+    private final List<SubProviderEntry> decoration_loot_tables = ImmutableList.of(new LootTableProvider.SubProviderEntry(BdDecorationLootTables::new, LootContextParamSets.BLOCK));
 
     public BdBlockLootTableProvider(PackOutput output) {
         super(output, Set.of(), List.of());
@@ -27,12 +25,17 @@ public class BdBlockLootTableProvider extends LootTableProvider {
 
     @Override
     public List<SubProviderEntry> getTables() {
-        return block_loot_tables;
+        List<SubProviderEntry> allLootTables = new ArrayList<>();
+        allLootTables.addAll(block_loot_tables);
+        allLootTables.addAll(decoration_loot_tables);
+        return allLootTables;
     }
 
     @Override
     protected void validate(Map<ResourceLocation, LootTable> map, ValidationContext validationtracker) {
         map.forEach((p_218436_2_, p_218436_3_) -> p_218436_3_.validate(validationtracker));
     }
+
+
 
 }
