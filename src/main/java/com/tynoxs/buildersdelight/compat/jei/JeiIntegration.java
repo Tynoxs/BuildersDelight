8ed87d5da9fed39d22b0ad60ff1b170178d3b4be
Benjamin LE SIRE <benjaminlesire@yahoo.com>
2024-02-05 11:59:30 +0100

    Fix ability to browse chisel recipes

--- src/main/java/com/tynoxs/buildersdelight/compat/jei/JeiIntegration.java
+++ src/main/java/com/tynoxs/buildersdelight/compat/jei/JeiIntegration.java
@@ -1,13 +1,16 @@
 package com.tynoxs.buildersdelight.compat.jei;
 
 import com.tynoxs.buildersdelight.BuildersDelight;
+import com.tynoxs.buildersdelight.content.init.BdItems;
 import com.tynoxs.buildersdelight.content.recipe.ChiselRecipe;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 
 @JeiPlugin
 public class JeiIntegration implements IModPlugin {
@@ -21,14 +24,16 @@ public class JeiIntegration implements IModPlugin {
 
     @Override
     public void registerCategories(IRecipeCategoryRegistration registration) {
-        IModPlugin.super.registerCategories(registration);
         registration.addRecipeCategories(new ChiselRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
     }
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
-        IModPlugin.super.registerRecipes(registration);
         registration.addRecipes(CHISEL_RECIPE_TYPE, BuildersDelight.get().getRecipeFactory().getChiselRecipes());
+    }
 
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration){
+        registration.addRecipeCatalyst(new ItemStack(BdItems.IRON_CHISEL.get()), CHISEL_RECIPE_TYPE);
     }
 }
